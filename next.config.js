f5c26caef87d43b9c4a5b64a65e20f16c59012e8
Akhil Singh Chauhan <akhil@bulsoft.com>
2022-11-04 15:18:04 +0530

    output path changed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'dist',
+  distDir: './build',
+  outDir: "./out"
 }
 
 module.exports = nextConfig
