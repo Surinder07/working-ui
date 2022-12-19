7aa0d61260bc529dcc86efba4fd51ebf7c464c97
Akhil Singh Chauhan <akhil@bulsoft.com>
2022-12-19 21:53:44 +0530

    updated production URL

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:8080/api' // development api
-            : 'https://waaw-server-stage.azurewebsites.net' // production api
+            : 'https://staging-api.waaw.ca' // production api
     },
     env: {
         version: '1.0',
